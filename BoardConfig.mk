4538fd840d3254625465993eeda28a3d288cf1ad
Tengfei Fan <tengfeif@codeaurora.org>
2021-07-27 18:21:31 +0800

    dubai: make ramdisk compressed by lz4
    There is big benefit if use lz4 compress ramdisk for
    kernel boot KPI.
    
    Change-Id: I7fe3010e26a34e433b701afb22df86d3a9d9597f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,7 @@ BOARD_KERNEL_CMDLINE := \
     ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_USE_LZ4 := true
 KERNEL_DEFCONFIG := vendor/dubai-qgki_defconfig
 
 # Partitions - A/B
