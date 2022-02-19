8c5731503476f8f408a726bec4df5128a839a49c
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-19 21:21:49 +0200

    dubai: Import hals.conf from stock
    Change-Id: Iabee9a85df8fdde819c56146f808179dda6dc8ea

--- device.mk
+++ device.mk
@@ -356,6 +356,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
