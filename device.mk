2cc1449dda670e78767adf422dd35640abf282ee
Jake Weinstein <jake@aospa.co>
2022-10-14 04:02:24 -0500

    dubai: Remove lights HAL packages
    Already in display component
    
    Change-Id: I6f8b50c9de6083329588960f3a2b6c3b41c40ba7

--- device.mk
+++ device.mk
@@ -124,11 +124,6 @@ PRODUCT_PACKAGES += \
    android.hardware.keymaster@4.1 \
    android.hardware.keymaster@4.1.vendor
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.lights-service.qti \
-    lights.qcom
-
 # Manifests
 DEVICE_MANIFEST_FILE := device/motorola/dubai/manifest_yupik.xml
 DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
