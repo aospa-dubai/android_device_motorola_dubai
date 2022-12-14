d364c47f93037a1388186ec6bab79075efe49c5d
Kunmun <kunmun@aospa.co>
2022-12-14 20:01:36 +0530

    dubai: Import rootdir init files from stock
    • From dubai-user-12-S1RD32.55-94-c9d3aa-release-keys
    • Sync fstab naming with the final product getting copied out
    • Cleanup the Motorola mess while we're at it.
    
    Change-Id: I1237846337f036f5a282f37bdef3b3e7ccba2c7f

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += fastbootd
 # Add default implementation of fastboot HAL.
 PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
 
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
@@ -218,7 +218,26 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
 #Charger
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/charger_fw_fstab.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/charger_fw_fstab.qti:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fw_fstab.qti
+
+# Init
+PRODUCT_PACKAGES += \
+    charger_fw_fstab.qti \
+    fstab.default \
+    init.mmi.boot.sh \
+    init.mmi.charge_only.rc \
+    init.mmi.chipset.rc \
+    init.mmi.overlay.rc \
+    init.mmi.rc \
+    init.mmi.touch.sh \
+    init.oem.hw.sh \
+    init.qcom.sensors.sh \
+    init.qti.chg_policy.sh \
+    init.qti.qcv.rc \
+    init.qti.qcv.sh \
+    init.target.rc \
+    init.vendor.st21nfc.rc \
+    ueventd.dubai.rc
 
 PRODUCT_ENABLE_QESDK := true
 
